0c1b6798906fb30b48b9ee0c689bea167be2acbe
Ibrahim Elshabrawy <Ibrahimshabra11@gmail.com>
2026-08-17 17:46:33 +0300

    Delete project directory

--- project/app/core/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-APP_NAME = os.getenv("APP_NAME", "Student Performance AI")
-MODEL_PATH = os.getenv("MODEL_PATH", "models/student_model.pkl")
